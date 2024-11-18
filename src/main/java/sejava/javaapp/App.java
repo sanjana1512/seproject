a07756696a7835de56bd49aff2af7b401abacaff
Shruthi1902 <jerubandishruthee@gmail.com>
2024-11-18 15:14:28 +0530

    added files

---
+++ src/main/java/sejava/javaapp/App.java
@@ -0,0 +1,13 @@
+package sejava.javaapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
